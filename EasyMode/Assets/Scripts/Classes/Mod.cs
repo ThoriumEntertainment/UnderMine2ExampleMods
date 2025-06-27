384aa9ef670c9d9f4c1c554f2d68bf005041eb97
ThoriumG <150734969+ThoriumG@users.noreply.github.com>
2025-06-27 14:14:49 -0600

    Easy Mode Mod - initial commit

---
+++ EasyMode/Assets/Scripts/Classes/Mod.cs
@@ -0,0 +1,13 @@
+using Thor.Core;
+using UnityEngine;
+
+namespace Mod_f41b71faab70436c8f46dd8c6b5377cd
+{
+    public class Mod : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // If your Mod needs to run code on startup, put it here and add this component to Mod.prefab!
+        }
+    }
+}
