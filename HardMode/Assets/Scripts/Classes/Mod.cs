42e3363fc817eaeb33dc24de8f981cbab8658764
ThoriumG <150734969+ThoriumG@users.noreply.github.com>
2025-06-27 12:01:59 -0600

    Checking in files after first Unity run

---
+++ HardMode/Assets/Scripts/Classes/Mod.cs
@@ -0,0 +1,13 @@
+using Thor.Core;
+using UnityEngine;
+
+namespace Mod_ddf88184e367440189877626c055ac2e
+{
+    public class Mod : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // If your Mod needs to run code on startup, put it here and add this component to Mod.prefab!
+        }
+    }
+}
