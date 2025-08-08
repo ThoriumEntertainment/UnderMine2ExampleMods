7811d3a7ef56900cf25d918cda84ddd37d7fa739
ThoriumG <150734969+ThoriumG@users.noreply.github.com>
2025-08-08 14:20:28 -0600

    Added AddingEncountersDynamically example

---
+++ AddingEncountersDynamically/Assets/Scripts/Classes/Mod.cs
@@ -0,0 +1,13 @@
+using Thor.Core;
+using UnityEngine;
+
+namespace Mod_e27493c24d5347f894c02a48faf3a207
+{
+    public class Mod : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // If your Mod needs to run code on startup, put it here and add this component to Mod.prefab!
+        }
+    }
+}
---
+++ AddingEncountersDynamically/Assets/Textures/Examples/Readme.txt
@@ -0,0 +1,6 @@
+These icons were adapted from this project: https://alexs-assets.itch.io/16x16-rpg-item-pack
+
+To convert them to the correct format for UM2:
+
+1. Resize the image to 48x48
+2. Add 4 pixels of empty space to each side (making the final image 56x56)
