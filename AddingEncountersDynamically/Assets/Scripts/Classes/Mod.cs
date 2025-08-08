e010674ff1de6c61d40d50e12deda4c8dece4594
ThoriumG <150734969+ThoriumG@users.noreply.github.com>
2025-08-08 14:33:23 -0600

    Add scripting example for spawning set pieces in the hub

--- AddingEncountersDynamically/Assets/Scripts/Classes/Mod.cs
+++ AddingEncountersDynamically/Assets/Scripts/Classes/Mod.cs
@@ -5,9 +5,41 @@ namespace Mod_e27493c24d5347f894c02a48faf3a207
 {
     public class Mod : MonoBehaviour
     {
+        [SerializeField]
+        private PrefabReference<Encounter> m_spawnInEncounter;
+
+        [SerializeField]
+        private AssetReference<SetPieceData> m_setPieceToSpawn;
+
         private void Awake()
         {
-            // If your Mod needs to run code on startup, put it here and add this component to Mod.prefab!
+            // Fired the first time an encounter is entered
+            Services.Events.RegisterGameEvent(GameEventType.EncounterBegin, OnEncounterBegin);
+        }
+
+        private void OnEncounterBegin(GameEvent evt)
+        {
+            var encounter = evt.ObjectValue as Encounter;
+            if (encounter == null)
+            {                
+                return;
+            }
+            
+            if (encounter.GUID == m_spawnInEncounter.Asset.GUID)
+            {                
+                if (!Services.Entities.Spawn(
+                    new IEntityService.SpawnParams()
+                    {
+                        data = m_setPieceToSpawn.Asset,
+                        scale = Vector3.one,
+                        position = Vector3.zero,                        
+                    },
+                    out var spawned
+                ))
+                {
+                    Debug.LogError($"[Mod] Failed to spawn set piece");
+                }
+            }
         }
     }
 }
