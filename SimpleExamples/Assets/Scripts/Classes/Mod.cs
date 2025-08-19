b3625fcdd6fd42c9eb5fbff1a0968a6156acbbbf
ThoriumG <150734969+ThoriumG@users.noreply.github.com>
2025-08-19 12:55:47 -0600

    initial commit of SimpleExamples

---
+++ SimpleExamples/Assets/Scripts/Classes/Mod.cs
@@ -0,0 +1,13 @@
+using Thor.Core;
+using UnityEngine;
+
+namespace Mod_d4eb6a34bbed4ad3bc88870d9cef7b75
+{
+    public class Mod : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // If your Mod needs to run code on startup, put it here and add this component to Mod.prefab!
+        }
+    }
+}
---
+++ SimpleExamples/README.md
@@ -0,0 +1,2 @@
+# UM2ModTemplate
+Quick start template project for UnderMine2 mods
